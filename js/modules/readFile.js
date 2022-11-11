7211f32e59e7e59e1c9f6f823d2d41fb9408aab2
Sergei Orlov <fulgeat@gmail.com>
2022-11-11 19:48:06 +0300

    Status: Assembly. Added file reading module

---
+++ js/modules/readFile.js
@@ -0,0 +1,13 @@
+const readFile = (directory, nameFile) => {
+    const fs = require('fs');
+    const path = require('path');
+    const file = path.join(__dirname, directory, nameFile);
+
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) throw new Error(err)
+        console.log(`Successfully: File "${directory}${nameFile}" file read`)
+        console.log(data)
+    })
+}
+
+module.exports = readFile;
